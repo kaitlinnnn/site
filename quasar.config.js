e5ee3cef9f9cd7e1412413b5d963fa2e1c66da17
kaitlinnnn <kmhaynal@email.wm.edu>
2023-02-09 21:09:00 -0500

    update public path

--- quasar.config.js
+++ quasar.config.js
@@ -60,7 +60,7 @@ module.exports = configure(function (/* ctx */) {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      publicPath: "/site/",
+      publicPath: "site",
       // analyze: true,
       // env: {},
       // rawDefine: {}
